cd138d2eb69b93119a70598963db614f9a18fc51
Andrew Hallam <andrew.hallam.10@ucl.ac.uk>
2015-03-01 19:21:51 +0000

    Created a numpy boids. It runs but doesn't appear totally correct currently.

--- fast_boids.py
+++ fast_boids.py
@@ -3,53 +3,51 @@
 A deliberately bad implementation of [Boids](http://dl.acm.org/citation.cfm?doid=37401.37406)
 for use as an exercise on refactoring.
 """
-
+import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
 
 # Deliberately terrible code for teaching purposes
 
-boids_x=[random.uniform(-450,50.0) for x in range(50)]
-boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
-boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
+boids_x=np.array([random.uniform(-450,50.0) for x in range(50)])
+boids_y=np.array([random.uniform(300.0,600.0) for x in range(50)])
+boid_x_velocities=np.array([random.uniform(0,10.0) for x in range(50)])
+boid_y_velocities=np.array([random.uniform(-20.0,20.0) for x in range(50)])
 
-def update_boids(boids):
-	xs,ys,xvs,yvs=boids
-	# Fly towards the middle
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
-	# Fly away from nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
-				xvs[i]=xvs[i]+(xs[i]-xs[j])
-				yvs[i]=yvs[i]+(ys[i]-ys[j])
-	# Try to match speed with nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
-				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
-				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
-	# Move according to velocities
-	for i in range(len(xs)):
-		xs[i]=xs[i]+xvs[i]
-		ys[i]=ys[i]+yvs[i]
+def update_boids(xs, ys, xvs, yvs):
+    
+        
+        for i in range(xs.size):
+                for j in range(xs.size):
+                       distance=(xs[j]-xs[i])**2 + (ys[j]-ys[i])**2
+                       
+                       if distance < 100:
+                              xvs[i]+=(xs[i]-xs[j])+(xs[j]-xs[i])*0.01/len(xs)
+			      yvs[i]+=(ys[i]-ys[j])+(ys[j]-ys[i])*0.01/len(xs)
+                              
+                       else:
+                              xvs[i]+=(xs[j]-xs[i])*0.01/len(xs)
+			      yvs[i]+=(ys[j]-ys[i])*0.01/len(xs)
+       
+        for i in range(xs.size):
+                for j in range(xs.size):
+                       distance=(xs[j]-xs[i])**2 + (ys[j]-ys[i])**2
+                       if distance < 10000:
+                              xvs[i]+=(xvs[j]-xvs[i])*0.125/len(xs)
+			      yvs[i]+=(yvs[j]-yvs[i])*0.125/len(xs)
+                              
+		xs += xvs
+		ys += yvs
 
 
 figure=plt.figure()
 axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
-scatter=axes.scatter(boids[0],boids[1])
+scatter=axes.scatter(boids_x,boids_y)
 
 def animate(frame):
-   update_boids(boids)
-   scatter.set_offsets(zip(boids[0],boids[1]))
+   update_boids(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
+   scatter.set_offsets(zip(boids_x,boids_y))
 
 
 anim = animation.FuncAnimation(figure, animate,
